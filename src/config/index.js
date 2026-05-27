d4cb5daa9ea58c6c8b69d2b599b839369b95cf2e
chhumsira-kh <sirachhum.kh@gmail.com>
2026-05-28 00:37:09 +0700

    new ssl

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,17 @@ import { Pool } from "pg";
 dotenv.config();
 
 const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DATABASE_URL, PORT, NODE_ENV } = process.env;
-
+const dbConfig = process.env.DATABASE_URL
+  ? { url: process.env.DATABASE_URL, dialect: "postgres", dialectOptions: { ssl: { rejectUnauthorized: false } } }
+  : {
+      host: process.env.db_host,
+      port: process.env.db_port,
+      username: process.env.db_user,
+      password: process.env.db_password,
+      database: process.env.db_name,
+      dialect: "postgres",
+      dialectOptions: { ssl: { rejectUnauthorized: false } }, // optional, depending on provider
+    };
 export const db = DATABASE_URL
   ? new Pool({ connectionString: DATABASE_URL })
   : new Pool({
